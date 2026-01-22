cc7c1016214a1fbbd03a8993887b8ce864186936
ZakareeyaM <moodleyzakareeya@gmail.com>
2026-01-22 09:40:29 +0200

    Delete Vercel directory

--- Vercel/script.js
+++
@@ -1,6 +0,0 @@
-const button = document.getElementById("btn");
-const message = document.getElementById("message");
-
-button.addEventListener("click", () => {
-    message.textContent = "🎉 JavaScript is working on Vercel!";
-});
